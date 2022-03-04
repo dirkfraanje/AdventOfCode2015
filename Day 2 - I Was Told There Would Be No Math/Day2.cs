abfe8f2cd5ee55dd114af739398b2e6904f04cba
Dirk Fraanje <46317263+dirkfraanje@users.noreply.github.com>
2022-03-04 11:01:52 +0100

    Day 2

--- Day 2 - I Was Told There Would Be No Math/Day2.cs
+++ Day 2 - I Was Told There Would Be No Math/Day2.cs
@@ -11,7 +11,7 @@ namespace AdventOfCode2015
         readonly static string[] input = System.IO.File.ReadAllLines("DayInputs/Day2.txt");
         public static object Result1()
         {
-            long result = 0;
+            int result = 0;
             foreach (var present in input)
             {
                 var measurements = present.Split('x').Select(Int32.Parse).ToArray();
@@ -20,7 +20,18 @@ namespace AdventOfCode2015
                 var side3 = measurements[1] * measurements[2];
 
                 var slack = new List<int>() { side1, side2, side3 }.Min();
-                result += 2*side1+ 2*side2 + 2*side3 + slack;
+                result += 2 * side1 + 2 * side2 + 2 * side3 + slack;
+            }
+            return result;
+        }
+
+        public static object Result2()
+        {
+            int result = 0;
+            foreach (var present in input)
+            {
+                var measurements = present.Split('x').Select(Int32.Parse).ToArray();
+                result += measurements.OrderBy(x => x).Take(2).Sum() * 2 + (measurements[0] * measurements[1] * measurements[2]);
             }
             return result;
         }
--- Program.cs
+++ Program.cs
@@ -5,3 +5,5 @@ Console.WriteLine();
 Console.WriteLine($"Result Day 1, part 2: {Day1.Result2()}");
 Console.WriteLine();
 Console.WriteLine($"Result Day 2, part 1: {Day2.Result1()}");
+Console.WriteLine();
+Console.WriteLine($"Result Day 2, part 2: {Day2.Result2()}");
